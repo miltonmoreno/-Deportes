52b8d500305adc40ce911492d2ce6c418fd39e23
Milton Moreno <miltonmoreno1608@gmail.com>
2024-09-03 03:06:49 -0500

    proyectoFinal

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-let productos = [
-    {id: "001924" , nombre: "Camiseta de Universitario de Deportes", categoría: "ropa", precio: 80},
-    {id: "001945" , nombre: "Short", categoría: "ropa", precio: 40},
-    {id: "002023" , nombre: "Zapatillas", categoría: "calzado", precio: 100},
-    {id: "001999" , nombre: "Pelota Eurocopa 2024", categoría: "accesorios", precio: 20},
-    {id: "002024" , nombre: "Gafas de natación Arena", categoría: "accesorios", precio: 40},
-    {id: "002045" , nombre: "Gafas de natación Winner", categoría: "accesorios", precio: 10},
-    {id: "002019" , nombre: "Medias antideslizantes", categoría: "ropa", precio: 10},
-    {id: "002015" , nombre: "Mangas de compresión", categoría: "ropa", precio: 30},
-    {id: "002014" , nombre: "Montura deportiva", categoría: "accesorios", precio: 60},
-]
+fetch("./info.json")
+    .then(response => response.json())
+    .then(productos => principal(productos))
+    .catch(error => error(error))
 
 function principal (productos){
     let buscador = document.getElementById("buscador")
     let botonBuscar = document.getElementById("botonBuscar")
+    let botonHombres = document.getElementById("hombres")
+    let botonMujeres = document.getElementById("mujeres")
+    let botonTodos = document.getElementById("botonTodos")
+    let botonInicio = document.getElementById("inicio")
     let cesta = crearCesta ()
     renderCesta(cesta)
 
@@ -20,37 +17,43 @@ function principal (productos){
     verCesta.addEventListener("click", ocultarCesta)
     botonBuscar.addEventListener("click", () => filtrarProductos(productos, buscador.value.toLowerCase()))
     buscador.addEventListener("keydown", function(tecla){
-        if(tecla.key == "Enter"){
-            filtrarProductos(productos,buscador.value.toLowerCase())
+        tecla.key == "Enter" && filtrarProductos(productos,buscador.value.toLowerCase())
         }
-    })
-    creadorElementos(productos)
-    filtrarProductos(productos,buscador.value)
+    )
+    botonInicio.addEventListener("click", paginaInicio)
+    botonTodos.addEventListener("click", () => creadorElementos(productos))
+    botonHombres.addEventListener("click", () => filtroGenero(productos, "hombre"))
+    botonMujeres.addEventListener("click", () => filtroGenero(productos, "mujer"))
+    // filtrarProductos(productos,buscador.value)
     let botonComprar = document.getElementById("botonComprar")
-    botonComprar.addEventListener("click", () => comprar(cesta))
+    botonComprar.addEventListener("click", () => comprar(crearCesta()))
+    console.log(cesta.length)
 }
 
 function ocultarCesta (e) {
-
+    let inicio = document.getElementById("paginaInicio")
     let contenedorProductos = document.getElementById("contenedor")
     let cajaCesta = document.getElementById("paginaCesta")
 
-    if(e.target.innerText == "Ver cesta"){
-        e.target.innerText = "Seguir comprando"
-    } else {
-        e.target.innerText = "Ver cesta"
-    }
+    e.target.innerText == "Ver cesta" ? e.target.innerText = "Seguir comprando" : e.target.innerText = "Ver cesta"
 
+    inicio.classList.toggle("oculto")
     contenedorProductos.classList.toggle("oculto")
+    cajaCesta.removeAttribute("style")
     cajaCesta.classList.toggle("oculto")
 }
 
 function creadorElementos (productos){
+    let inicio = document.getElementById("paginaInicio")
+    inicio.innerHTML=`
+    `
     let contenedorProductos = document.getElementById("contenedor")
-    contenedorProductos.innerHTML = ""
+    contenedorProductos.removeAttribute("style")
+    contenedorProductos.innerHTML= ""
     productos.forEach(producto => {
         contenedorProductos.innerHTML += `
             <div class=producto>
+                <img src=${producto.imagen} class=imagenProducto>
                 <h3>${producto.nombre}</h3>
                 <p>$${producto.precio}</p>
                 <button id="${producto.id}">Agregar a la cesta</button>
@@ -65,30 +68,33 @@ function creadorElementos (productos){
 }
 
 function comprar (cesta){
-    let contenedorPopup = document.getElementById("paginaCesta")
     if(cesta.length != 0){
         localStorage.removeItem("cesta")
         renderCesta([])
-        contenedorPopup.innerHTML= `
-        <div class=popUp>
-            <h3>Gracias por tu compra. Sigue en movimiento.</h3>
-            <button id=botonCerrar>Cerrar</button>
-        </div>
-        `
+        compraFinalizada("Gracias por tu compra")
     } else {
-            contenedorPopup.innerHTML= "<h3>No has añadido nada a la cesta. Intenta de nuevo.</h3>"
+        compraErronea("No has añadido nada a la cesta")
     }
-
-        function cerrarPopup (contenedorPopup) {
-            contenedorPopup.innerHTML = ""
-        }
-    
-        let botonCerrar = document.getElementById("botonCerrar")
-        botonCerrar.addEventListener("click", () => cerrarPopup(contenedorPopup))
 }
 
 function filtrarProductos (productos, valor){
+    let contenedorProductos = document.getElementById("contenedor")
+    contenedorProductos.removeAttribute("style", "display:none")
+    let inicio = document.getElementById("paginaInicio")
+    inicio.setAttribute("stye", "none")
     let productosFiltrados = productos.filter(producto => producto.nombre.toLowerCase().includes(valor))
+    creadorElementos(productosFiltrados)
+
+    if (productosFiltrados.length === 0) {
+        let contenedorProductos = document.getElementById("contenedor");
+        contenedorProductos.innerHTML = `
+            <h3>¡Ups! No encontramos lo que buscas</h3>
+        `;
+    }
+}
+
+function filtroGenero (productos,genero){
+    let productosFiltrados = productos.filter(producto => producto.genero.toLowerCase() == genero || producto.genero.toLowerCase() == "ambos")
     creadorElementos(productosFiltrados)
 
     if (productosFiltrados.length === 0) {
@@ -139,15 +145,41 @@ function renderCesta (cesta){
     cajaCesta.innerHTML = ""
 
     cesta.forEach(producto => {
+
         cajaCesta.innerHTML += `
-            <div class=articuloCesta>
+            <div class=articuloCesta id=${producto.id}>
                 <p>${producto.nombre}</p>
                 <p>${producto.precio}</p>
                 <p>${producto.cantidad}</p>
                 <p>${producto.subtotal}</p>
+                <button id=eliminar${producto.id}>Eliminar</button>
             </div>
         `
     })
+
+    cesta.forEach(producto => {
+        let botonEliminar = document.getElementById("eliminar" + producto.id)
+        botonEliminar.addEventListener("click", (e) => eliminarProducto(e))
+    })
+
+    let sumaSubtotal = cesta.reduce((total, producto) => total + producto.subtotal, 0 )
+
+    let totalCesta = document.getElementById("totalCesta")
+    totalCesta.innerHTML=`
+            <p>Total: ${sumaSubtotal}</p>
+        `
+
+}
+
+function eliminarProducto(e){
+    let id = e.target.id.substring(8)
+    let cesta = crearCesta()
+
+    cesta = cesta.filter(producto => producto.id !== id)
+
+    guardarCesta(cesta)
+
+    renderCesta(cesta)
 }
 
 function guardarCesta(cesta) {
@@ -156,19 +188,50 @@ function guardarCesta(cesta) {
 }
 
 function popupCesto (producto){
-    let contenedorPopups = document.getElementById("contenedorPopups")
-    contenedorPopups.innerHTML = `
-    <div class=popUp>
-        <h3>Agregaste ${producto.nombre} a la cesta</h3>
-        <button id=botonCerrar>Cerrar</button>
-    </div>
-`
-    function cerrarPopup () {
-        contenedorPopups.innerHTML = ""
-    }
-
-    let botonCerrar = document.getElementById("botonCerrar")
-    botonCerrar.addEventListener("click", cerrarPopup)
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Añadiste " + producto.nombre + " a la cesta.",
+        showConfirmButton: false,
+        timer: 1500
+      });
 }
 
-principal(productos)
+function compraFinalizada (mensaje){
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: mensaje,
+        showConfirmButton: false,
+        timer: 1500
+      });
+}
+
+function compraErronea (mensaje){
+    Swal.fire({
+        icon: "error",
+        title: "No has añadido nada a la cesta",
+        text: "Sigue comprando",
+      });
+}
+
+function error (mensaje){
+    Swal.fire({
+        icon: "error",
+        title: mensaje,
+        text: "Inténtalo de nuevo",
+      });
+}
+
+function paginaInicio () {
+    let inicio = document.getElementById("paginaInicio")
+    inicio.innerHTML=`
+        <div>
+            <h1>Dunkel</h1>
+            <h2>El deporte es vida. Vívelo bien equipado.</h2>
+            <img src="assets/inicio.jpg" alt="Hombre nadando" id="imagenInicio">
+        </div>
+    `
+    let contenedorProductos = document.getElementById("contenedor")
+    contenedorProductos.setAttribute("style", "display:none")
+}
